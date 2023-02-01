0fa5963beef0bb334b4f59c46eaf50ba7b8453b9
donovan <donoriviere@gmail.com>
2023-02-01 09:45:54 +0100

    initServer

---
+++ app.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+require('express-async-errors');
+const cors = require('cors');
+
+const express = require('express');
+const app = express();
+n;
+
+app.listen(5000, () => {
+  console.log('Le serveur écoute sur le port 5000...');
+});
