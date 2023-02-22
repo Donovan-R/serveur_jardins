082bf5877af8d3eea850f05c24d7c35a1b93b815
donovan <donoriviere@gmail.com>
2023-02-22 20:18:39 +0100

    addadmin

--- controllers/dash.js
+++ controllers/dash.js
@@ -7,6 +7,32 @@ const getUsers = async (req, res) => {
   res.status(StatusCodes.OK).json({ data });
 };
 
+const deleteUser = async (req, res) => {
+  const { id: userID } = req.params;
+  console.log(userID);
+  const { rows: data } = await db.query(
+    'DELETE FROM users WHERE user_id = $1 RETURNING *',
+    [userID]
+  );
+  res.status(StatusCodes.OK).json({ data });
+};
+
+const editUser = async (req, res) => {
+  const {
+    userToEdit: { lastname, firstname, mobile, email, role_id },
+  } = req.body;
+  const { id } = req.params;
+  const {
+    rows: [user],
+  } = await db.query(
+    'UPDATE users SET lastname = $1, firstname = $2, mobile = $3, email = $4, role_id =$5 WHERE user_id = $6 RETURNING *',
+    [lastname, firstname, mobile, email, role_id, id]
+  );
+  res.status(StatusCodes.OK).json({ user });
+};
+
 module.exports = {
   getUsers,
+  deleteUser,
+  editUser,
 };
--- routes/dash.js
+++ routes/dash.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const { getUsers } = require('../controllers/dash.js');
+const { getUsers, deleteUser, editUser } = require('../controllers/dash.js');
 
 router.route('/').get(getUsers);
+router.route('/:id').delete(deleteUser).put(editUser);
 
 module.exports = router;
